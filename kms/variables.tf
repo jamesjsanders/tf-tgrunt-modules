5f00d69776cdce4681a2a61be91e995ea0aceec7
jamesjsanders <james.sanders@15five.com>
2019-11-05 14:23:42 -0800

    first commit

---
+++ kms/variables.tf
@@ -0,0 +1,68 @@
+variable "namespace" {
+  type        = string
+  description = "Namespace (e.g. `eg` or `cp`)"
+  default     = ""
+}
+
+variable "stage" {
+  type        = string
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
+  default     = ""
+}
+
+variable "name" {
+  type        = string
+  description = "Application or solution name (e.g. `app`)"
+}
+
+variable "delimiter" {
+  type        = string
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `stage`, `name` and `attributes`"
+}
+
+variable "attributes" {
+  type        = list(string)
+  default     = []
+  description = "Additional attributes (e.g. `1`)"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
+}
+
+variable "deletion_window_in_days" {
+  default     = 10
+  description = "Duration in days after which the key is deleted after destruction of the resource"
+}
+
+variable "enable_key_rotation" {
+  type        = bool
+  default     = true
+  description = "Specifies whether key rotation is enabled"
+}
+
+variable "description" {
+  type        = string
+  default     = "Parameter Store KMS master key"
+  description = "The description of the key as viewed in AWS console"
+}
+
+variable "alias" {
+  type        = string
+  default     = ""
+  description = "The display name of the alias. The name must start with the word `alias` followed by a forward slash"
+}
+variable "enabled" {
+  type        = bool
+  default     = true
+  description = "Set to false to prevent the module from creating any resources"
+}
+
+variable "policy" {
+  type        = string
+  default     = ""
+  description = "A valid KMS policy JSON document. Note that if the policy document is not specific enough (but still valid), Terraform may view the policy as constantly changing in a terraform plan. In this case, please make sure you use the verbose/specific version of the policy."
+}
